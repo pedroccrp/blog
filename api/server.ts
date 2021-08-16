e61e68c919293732d9d8b8ad12eea04b76293b5d
Pedro Pontes <pontescpedro@gmail.com>
2021-08-16 17:53:26 -0300

    Add entry files and install base modules

---
+++ api/server.ts
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv';
+
+import app from 'src/app';
+// import database from 'src/database';
+
+dotenv.config({
+  path: './src/.env',
+});
+
+// database.connect();
+
+const HOST = process.env.HOST || '0.0.0.0';
+const PORT = process.env.PORT || '5000';
+
+app.listen(parseInt(PORT), HOST, () =>
+  console.log(`[server] listening on port ${PORT}`),
+);
