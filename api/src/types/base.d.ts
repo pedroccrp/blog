80a21901215e49638ed5a25998f9d14d21a5f982
Pedro Pontes <pontescpedro@gmail.com>
2021-08-19 17:23:53 -0300

    Updated types for api responses

--- api/src/types/base.d.ts
+++ api/src/types/base.d.ts
@@ -1,8 +1,9 @@
 export interface ApiResponse {
   statusCode: number;
-
-  error: boolean;
-  message?: string;
-
   data?: {};
 }
+
+export type ApiResponseError =
+  | { type: 'database-error'; message: string }
+  | { type: 'internal-error'; message: string }
+  | { type: 'wrong-request'; message: string };
--- api/src/types/user.d.ts
+++ api/src/types/user.d.ts
@@ -1,8 +1,13 @@
 import { UserData } from 'src/models/user';
-import { ApiResponse } from 'src/types/base';
+import { ApiResponse, ApiResponseError } from 'src/types/base';
 
 interface UserResponse extends ApiResponse {
   data?: UserResponseData;
 }
 
-interface UserResponseData extends UserData {}
+interface UserResponseData {
+  error?: UserResponseError;
+  user?: UserData;
+}
+
+type UserResponseError = ApiResponseError;
